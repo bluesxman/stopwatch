ec7532b0f1231877f6b87aaf200ceeaf1b97b513
Jon Newton <jon.newton@gmail.com>
2015-02-28 07:48:06 -0700

    deployment tweaks

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject stopwatch "0.1.0-SNAPSHOT"
+(defproject stopwatch "0.1.0"
   :description "A small, simple, composable library for timing expression evaluation."
   :url "https://github.com/bluesxman/stopwatch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/bluesxman/stopwatch"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
