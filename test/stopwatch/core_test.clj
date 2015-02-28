4a9354dee12c2b1af6dea7f6a5c857d96325b1c4
Jon Newton <jon.newton@gmail.com>
2015-02-28 10:31:32 -0700

    tests

--- test/stopwatch/core_test.clj
+++ test/stopwatch/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [stopwatch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest same-result
+  (is (= (nanof identity (+ 1 2 3)) (+ 1 2 3)))
+  (let [[t v] (nanov (str 2 3 4 {:a 1} ["blah"]))]
+    (is (= v (str 2 3 4 {:a 1} ["blah"])))
+    (is (not (neg? t)))))
+
+(deftest valid-time
+  (is (not (neg? (nano (sort [6 23 7 3 5]))))))
+
+
