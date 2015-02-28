4497cbc625392478cf4529ca97b2714ab84c0c96
Jon Newton <jon.newton@gmail.com>
2015-02-27 17:25:28 -0700

    core macros

--- src/stopwatch/core.clj
+++ src/stopwatch/core.clj
@@ -1,6 +1,25 @@
 (ns stopwatch.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro nano
+  "Returns the time to evaluate the expression in nanoseconds."
+  [expr]
+  `(let [start# (System/nanoTime)]
+     ~expr
+     (- (System/nanoTime) start#)))
+
+(defmacro nanof
+  "Returns the result of the expression and also calls the function f on the
+  time (in nanoseconds) it took to evaluate the expression."
+  [f expr]
+  `(let [start# (System/nanoTime)
+         ret# ~expr]
+     (~f (- (System/nanoTime) start#))
+     ret#))
+
+(defmacro nanov
+  "Returns a vector where the first value is the time in nanoseconds to evaluate the
+  expression and the second value is the result of evaluating the expression."
+  [expr]
+  `(let [start# (System/nanoTime)
+         ret# ~expr]
+     [(- (System/nanoTime) start#) ret#]))
