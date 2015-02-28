867d1eefe3be4a49497b8fd9035e16e6a70023b5
Jon Newton <jon.newton@gmail.com>
2015-02-27 18:37:55 -0700

    Added info to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject stopwatch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small, simple, composable library for timing expression evaluation."
+  :url "https://github.com/bluesxman/stopwatch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
